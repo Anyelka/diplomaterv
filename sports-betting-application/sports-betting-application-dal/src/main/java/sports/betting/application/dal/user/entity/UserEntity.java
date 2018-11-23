cc8b3a22bece3c9fe431022e452cbc63614bf8c4
Marton Takacs <v-mtakacs@expedia.com>
2018-11-23 19:26:46 +0100

    revert commit

--- sports-betting-application/sports-betting-application-dal/src/main/java/sports/betting/application/dal/user/entity/UserEntity.java
+++ sports-betting-application/sports-betting-application-dal/src/main/java/sports/betting/application/dal/user/entity/UserEntity.java
@@ -12,9 +12,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Version;
 
 @Entity
+@Table(name = "user")
 public class UserEntity {
 
     @Id
